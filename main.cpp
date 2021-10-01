07fab1f257c14db2ffae981540c92a29b2bc237e
Carson Baughan <carsonbaughan@gmail.com>
2021-09-30 21:16:28 -0600

    got rid of endl in main

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
         parser->Run(lexer->GetTokens());
     }
     catch(std::string e){
-        std::cout << e << std::endl;
+        std::cout << e;
     }
 
 
